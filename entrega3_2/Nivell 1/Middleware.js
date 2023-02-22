20c9ac4e3e3b90d2c73c9eef79473c60f00ff362
David Iglesias <im.david85@gmail.com>
2023-02-22 10:43:36 +0100

    middleware review

--- entrega3_2/Nivell 1/Middleware.js
+++ entrega3_2/Nivell 1/Middleware.js
@@ -4,13 +4,6 @@ class Middleware {
     this.middlewares = [];
     this.req = {};
 
-    /*
-    Para obtener las funciones del objeto Target e invocarlas, es necesario crearlas dinámicamente en el Manager importándolas.
-    Para ello:
-      -   Se obtiene el prototype del objeto target
-      -   Se crea la función dinámicamente (que no sea el constructor)
-      -   Posteriormente se mapean los valores de entrada al Manager con los parámetros necesarios en cada invocación
-*/
     const prototype = Object.getPrototypeOf(this.target);
     Object.getOwnPropertyNames(prototype).forEach((fn) => {
       if (fn !== "constructor") return this.createFn(fn);
@@ -28,6 +21,7 @@ class Middleware {
   }
   createFn(fn) {
     this[fn] = (args) => {
+      console.log(args);
       this.req = args;
       this.executeMiddleware();
       return this.target[fn].call(this, this.req);
--- entrega3_2/Nivell 1/app.js
+++ entrega3_2/Nivell 1/app.js
@@ -1,8 +1,9 @@
 const { Calculadora } = require("./Claculadora");
 const { Middleware } = require("./Middleware");
 const params = require("./params.json");
+
 const calcu = new Calculadora();
-const mid = new Middleware(calcu);
+const app = new Middleware(calcu);
 
 function cuadrado(z) {
   z.x = z.x * z.x;
@@ -21,19 +22,27 @@ function division(z) {
 }
 
 // modificamos {objeto enviado}
-mid.use((req, next) => {
-  req = cuadrado(req);
-  console.log("cuadrado", req);
-  req = cubo(req);
-  console.log("cubo", req);
-  req = division(req);
-  console.log("division", req);
-  next(); //llama a middleware
+app.use((data, next) => {
+  cuadrado(data);
+  console.log("cuadrado", data);
+  next();
 });
 
-console.log("SUMA", mid.sumar({ x: params.dos, y: params.tres }));
-console.log("RESTA", mid.restar({ x: params.tres, y: params.dos }));
-console.log(
-  "MULTIPLICACION",
-  mid.multiplicar({ x: params.tres, y: params.tres })
-);
+app.use((data, next) => {
+  cubo(data);
+  console.log("cubo", data);
+  next();
+});
+
+app.use((data, next) => {
+  division(data);
+  console.log("division", data);
+  next();
+});
+
+console.log("SUMA", app.sumar({ x: params.dos, y: params.tres }));
+// console.log("RESTA", app.restar({ x: params.tres, y: params.dos }));
+// console.log(
+//   "MULTIPLICACION",
+//   app.multiplicar({ x: params.tres, y: params.tres })
+// );
