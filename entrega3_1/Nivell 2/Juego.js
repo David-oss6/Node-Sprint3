3bd466c3f1ffe52f0ad3413fc51da5ff4e9ea421
David Iglesias <im.david85@gmail.com>
2023-03-17 11:33:01 +0100

    rehaciendo singleton

--- entrega3_1/Nivell 2/Juego.js
+++ entrega3_1/Nivell 2/Juego.js
@@ -1,31 +1,37 @@
+const { Marcador } = require("./Marcador")
+
 class Juego {
-  constructor(x, y, marcador) {
-    this.x = x;
-    this.y = y;
-    this.marcador = marcador
+  constructor(x, y, nombre) {
+    this.x = x
+    this.y = y
+    this.juego = nombre
+    this.marcador = new Marcador(this.x, this.y, nombre)
+    console.log("marcador:::", this.marcador)
   }
   chutar() {
-    console.log(`${this.x.nombre} chuta!!!`);
     let chuteX = Math.floor(Math.random() * 300)
-    console.log(`distancia: ${chuteX} metros`);
-    console.log(`${this.y.nombre} chuta!!!`)
     let chuteY = Math.floor(Math.random() * 300)
-    console.log(`distancia: ${chuteY} metros`)
+    console.log(`${this.x.nombre} chuta! Distancia: ${chuteX} metros`)
+    console.log(`${this.y.nombre}! Distancia: ${chuteY} metros`)
 
     if (chuteX > chuteY) {
       this.marcador = {
-        ...this.marcador, paco: this.marcador.paco + 1
+        ...this.marcador,
+        jugadorA: this.marcador.jugadorA + 1,
       }
       console.log(`punto para: ${this.x.nombre}`)
     } else {
       this.marcador = {
-        ...this.marcador, david: this.marcador.david + 1
+        ...this.marcador,
+        JugadorB: this.marcador.JugadorB + 1,
       }
       console.log(`punto para: ${this.x.nombre}`)
-
     }
-    console.log(`*** Resultado: ${this.marcador.paco} - ${this.marcador.david} *** `)
+    console.log(
+      `*** Resultado: ${this.marcador.jugadorA} - ${this.marcador.JugadorB} *** `
+    )
+    console.log("MARCADOR", this.marcador)
   }
 }
 
-module.exports = { Juego };
+module.exports = { Juego }
--- entrega3_1/Nivell 2/app.js
+++ entrega3_1/Nivell 2/app.js
@@ -1,21 +1,25 @@
-const { Jugador } = require("./Jugador");
-const { Juego } = require("./Juego");
+const { Jugador } = require("./Jugador")
+const { Juego } = require("./Juego")
 const { Marcador } = require("./Marcador")
 
-const paco = new Jugador("Paco");
-const david = new Jugador("David");
-const marcador = new Marcador(0, 0)
+const paco = new Jugador("Paco")
+const david = new Jugador("David")
+const sonia = new Jugador("Sonia")
+const maripili = new Jugador("Maripili")
 
 const empezarJuego = () => {
-  penaltis = new Juego(paco, david, marcador);
-  console.log(penaltis);
-  penaltis.chutar();
+  const penaltis = new Juego(paco, david, "juego1")
+  const juego2 = new Juego(sonia, maripili, "juego2")
+
+  penaltis.chutar()
+  juego2.chutar()
   setTimeout(() => {
     penaltis.chutar()
+    juego2.chutar()
   }, [2000])
   setTimeout(() => {
     penaltis.chutar()
+    juego2.chutar()
   }, [4000])
-};
-empezarJuego();
-
+}
+empezarJuego()
